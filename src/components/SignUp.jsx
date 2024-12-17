d8605fec089e72476f432cc79f393086a1851e31
shafriki <shafriki.cse@gmail.com>
2024-12-17 18:19:50 +0600

    update security

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,62 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
+import auth from './firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignUp = () => {
+
+    const handleSignUp = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        if(password.length <6) {
+            toast.error(`pass min 6 character`);
+            return;
+            }
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+            console.log(result);
+            toast.success("Signup successful!");
+        })
+        .catch((error) => {
+            console.log('error', error);
+            toast.error(`Error: ${error.message}`);
+        });
+        
+    };
+
+    
+
     return (
-        <div>
-            signUP
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <ToastContainer />
         </div>
     );
 };
--- src/components/firebase.init.js
+++ src/components/firebase.init.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlUB_gMXwG1afh4fE4act7fJDFlWuvDFg",
-  authDomain: "emailpass-auth-6de4a.firebaseapp.com",
-  projectId: "emailpass-auth-6de4a",
-  storageBucket: "emailpass-auth-6de4a.firebasestorage.app",
-  messagingSenderId: "580121538287",
-  appId: "1:580121538287:web:be70308b03745cfafa351a"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
